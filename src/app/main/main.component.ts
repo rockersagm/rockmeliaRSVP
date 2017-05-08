62564d0624a3a7028983de948023ef1be7eb338d
amanetakis@gmail.com <amanetakis@gmail.com>
2017-05-08 21:23:26 +1000

    countdown timer & venues work

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -5,6 +5,8 @@ import { SFService }          	from '../sf.service';
 import { Family, Contact }      from '../Family';
 
 import * as moment from 'moment';
+import {Observable} from 'rxjs/Rx';
+
 
 
 @Component({
@@ -16,6 +18,9 @@ export class MainComponent implements OnInit {
 
 	family : Family;
 	daysToGo : string;
+	hoursToGo : string;
+	minsToGo : string;
+	secsToGo : string;
 
 	constructor(private router: Router,
 				public authService: AuthService,
@@ -23,10 +28,23 @@ export class MainComponent implements OnInit {
 	}
 
 	ngOnInit() {
-		var wedding = moment("2017-10-28");
-		var now = moment();
-		this.daysToGo =  (wedding.diff(now, 'days')+1).toString();
 		this.family = this.sfService.loadData();
+		Observable.interval(1000)
+					.startWith(0)
+					.subscribe(x => {
+ 						this.updateCounter();
+	  				});
+	}
+
+
+	//TODO: make more efficient
+	updateCounter() {
+		var wedding = moment("2017-10-28 16:00");
+		var now = moment();
+		this.daysToGo =  (wedding.diff(now, 'days')).toString();
+		this.hoursToGo =  ( wedding.diff(now, 'hours') - (wedding.diff(now, 'days') * 24) ).toString();
+		this.minsToGo = ( wedding.diff(now, 'minutes') - (wedding.diff(now, 'hours') * 60) ).toString();
+		this.secsToGo = ( wedding.diff(now, 'seconds') - (wedding.diff(now, 'minutes') * 60) ).toString();
 	}
 
 	gotoLogout() {
